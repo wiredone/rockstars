542734f2b5d2c87eebf4e6eb932c9f250601ccdd
David MacKintosh <dgawmackintosh@gmail.com>
2016-06-19 22:32:02 +0100

    Alters functions in API service file

--- client/src/gig_map/eventful_service.js
+++ client/src/gig_map/eventful_service.js
@@ -2,40 +2,48 @@ var _ = require('lodash');
 
 var ApiService = function() {
 
-  this.url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=evSfYBzBfoEQwQq13yf0I0Po7YGf2W&";
+  this.url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=evSfYBzBfoEQwQq13yf0I0Po7YGf2W";
   this.radius = "&radius=2&unit=miles";
-  this.latLong = {lat: 0, long: 0};
+  this.latLong = "";
   this.genre = "";
-  this.startDate = new Date();
-  this.endDate = new Date();
+  this.dateRange = "";
 
 
   this.setLatLng = function(lat, long) {
     var latString = _.toString(lat);
     var longString = _.toString(long);
-    var latLongString = latString + "," + longString;
-    this.url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=evSfYBzBfoEQwQq13yf0I0Po7YGf2W&latlong=" + latLongString + this.radius;
+    this.latLong = "&latlong=" + latString + "," + longString + this.radius + "&";
 
-    return this.url;
+    return this.latLong;
   };
 
 
   this.setGenre = function(genre) {
-    var genreString = _.toString(genre);
-    this.url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=evSfYBzBfoEQwQq13yf0I0Po7YGf2W&classificationName=" + genreString;
+    var genreString = _.toString(genre)
+    this.genre = "&classificationName=" + genreString;
 
-    return this.url
+    return this.genre;
   };
 
+
+  //"2016-06-16"
 
   this.setDates = function(startDate, endDate) {
-    var startDateString = _.toString(startDate) + "T00:00:00Z";
-    var endDateString = _.toString(endDate) + "T00:00:00Z";
-    this.url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=evSfYBzBfoEQwQq13yf0I0Po7YGf2W&startDateTime=" + startDateString + "&endDateTime=" + endDateString;
+    var startString = "startDateTime=" + startDate + "T00:00:00Z&";
+    var endString = "endDateTime=" + endDate + "T23:59:59Z&";
+    this.dateRange = startString + endString
 
-    return this.url
+    return this.dateRange
   };
-  
+
+  //startDateTime=2016-08-01T00:00:00Z&
+  //endDateTime=2016-08-01T23:59:59Z&
+
+
+  this.createUrl = function() {
+
+  };
+
 
   this.getEvents = function(callback) {
     var request = new XMLHttpRequest();
