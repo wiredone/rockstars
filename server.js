29c393183496a396e9f8689b23a4831932235d2a
Matthew Edmondson <mle@wired.one>
2016-06-20 11:09:21 +0100

    adding basic server

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express');
+
+
+var path = require('path')
+
+
 var app = express();
-var path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-
+app.use(express.static('client/build'));
+//splash and sign up and login
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-app.use(express.static('client/build'));
-
-
-var server = app.listen(3000, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('rockstars are listening ', host, port);
+  console.log('Rockstars are listening', host, port);
 });
