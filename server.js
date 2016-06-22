67a5d362f7ef777a852a537dbe42723708d4c6c4
Matthew Edmondson <mle@wired.one>
2016-06-22 09:28:04 +0100

    freezing for develop commit

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var path = require('path')
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs'); // not sure if needed - mle
+var ObjectId = require('mongodb').ObjectID;
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
 
 
 app.use(bodyParser.json());
@@ -11,15 +14,29 @@ app.use(express.static('client/build'));
 
 var MongoClient = require('mongodb').MongoClient
 
-
-
 // Connection URL
 var url = 'mongodb://localhost:27017/gigmapper';
 
 //splash and sign up and login
 app.get('/', function (req, res) {
+  var cached = localStorage.getItem("user_ids"); //doesnae trigger
+  console.log(cached); //doesnae trigger
+  res.send('id: ' + req.query.id);
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
+//login
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/build/login.html'));
+  var cached = localStorage.getItem("user_ids");
+  if (cached){
+  console.log(cached);
+  }
+});
+
+app.get('/user', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/build/myaccount.html'));
+  // res.render(path.join(__dirname + '/client/build/myaccount'), {username: req.body.name})
 });
 
 // we need an xml post for the api data that is being uploaded to the database - what data is being uploaded? user and events.
@@ -37,17 +54,48 @@ app.get('/users', function(req,res){
   });
 })
 
+// app.post('/', function(req,res){
+// console.log("login post " + req.body.username + req.body.password);
+// MongoClient.connect(url, function(err, db) {
+//   var collection = db.collection('users');
+//   collection.insert(
+//     { "username": req.body.username,
+//       "password": req.body.password
+//     }
+//   );
+//   // dbUserObj=  collection.find(ObjectId("57687a2d9baaae4d4bbc8417"))
+//   // console.log(dbUserObj);
+//   res.status(200).end()
+//   db.close();
+// });
+
+
+
+// });
+
 app.post('/users', function(req,res){
-  console.log('body', req.body)
+  //console.log('body', req.body);
   MongoClient.connect(url, function(err, db) {
     var collection = db.collection('users');
-    collection.insert(
-      { "username": req.body.username,
-        "password": req.body.password
-      }
-    )
-    res.status(200).end()
+    var objectToInsert = req.body;
+    collection.insert(objectToInsert, function(err){
+      // if (err) return;
+    var objectid = objectToInsert._id;
+  //  console.log(objectid);
+    localStorage.setItem("user_ids",JSON.stringify(objectid));
+    res.redirect('/?user_id=' + objectid);
+    // res.redirect('/?user_id=' + objectid);
+
+
+    });
+    //res.status(200).end()
+    //console.log(newUser);
+
+
+    //redirrs
     db.close();
+
+     // does this actually close it?
   });
 })
 
