6f1c2a21bdb5c94d8a7335a11e0a0425d8ffa7c0
Matthew Edmondson <mle@wired.one>
2016-06-22 11:19:03 +0100

    some tidying up preparing for user objectmodel

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var ObjectId = require('mongodb').ObjectID;
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static('client/build'));
 
 var MongoClient = require('mongodb').MongoClient
@@ -16,88 +18,59 @@ var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017/gigmapper';
 
 //splash and sign up and login
-app.get('/', function (req, res) {
-  // var cached = localStorage.getItem("user_ids"); //doesnae trigger
-  // console.log(cached); //doesnae trigger
-
-  res.send('id: ' + req.query.id);
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get('/', function(req, res) {
+    res.send('id: ' + req.query.id);
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.get('/login', function (req, res) {
-  res.sendFile(path.join(__dirname + '/client/build/login.html'));
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/build/login.html'));
 
 });
 
-app.get('/user', function (req, res) {
-  res.sendFile(path.join(__dirname + '/client/build/myaccount.html'));
+app.get('/user', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/build/myaccount.html'));
 });
 
 // we need an xml post for the api data that is being uploaded to the database - what data is being uploaded? user and events.
 
 
 //question user for actual user or users - for admin
-app.get('/users', function(req,res){
+app.get('/users', function(req, res) {
 
-  MongoClient.connect(url, function(err, db) {
-    var collection = db.collection('users');
-    collection.find({}).toArray(function(err, docs) {
-      res.json(docs);
-      db.close();
+    MongoClient.connect(url, function(err, db) {
+        var collection = db.collection('users');
+        collection.find({}).toArray(function(err, docs) {
+            res.json(docs);
+            db.close();
+        });
     });
-  });
 })
 
-// app.post('/', function(req,res){
-// console.log("login post " + req.body.username + req.body.password);
-// MongoClient.connect(url, function(err, db) {
-//   var collection = db.collection('users');
-//   collection.insert(
-//     { "username": req.body.username,
-//       "password": req.body.password
-//     }
-//   );
-//   // dbUserObj=  collection.find(ObjectId("57687a2d9baaae4d4bbc8417"))
-//   // console.log(dbUserObj);
-//   res.status(200).end()
-//   db.close();
-// });
 
 
+app.post('/users', function(req, res) {
+    MongoClient.connect(url, function(err, db) {
+        var collection = db.collection('users');
+        var objectToInsert = req.body;
+        collection.insert(objectToInsert, function(err) {
+            // if (err) return;
+            var objectid = objectToInsert._id;
 
-// });
+            res.redirect('/?user_id=' + objectid);
+        });
 
-app.post('/users', function(req,res){
-  //console.log('body', req.body);
-  MongoClient.connect(url, function(err, db) {
-    var collection = db.collection('users');
-    var objectToInsert = req.body;
-    collection.insert(objectToInsert, function(err){
-      // if (err) return;
-    var objectid = objectToInsert._id;
-  //  console.log(objectid);
+        db.close();
 
-    // res.redirect('/?user_id=' + objectid);
-
-    res.redirect('/?user_id=' + objectid);
     });
-    //res.status(200).end()
-    //console.log(newUser);
-
-
-    //redirrs
-    db.close();
-
-     // does this actually close it?
-  });
 })
 
 
 
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Rockstars are listening', host, port);
+    console.log('Rockstars are listening', host, port);
 });
